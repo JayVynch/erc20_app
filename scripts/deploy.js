815d223ef07b5ce240bb1e20c74f14ee8164a203
Jacob George <kooldrjay060@gmail.com>
2024-11-05 14:40:20 +0100

    added deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,34 @@
+const hre  = require("hardhat");
+const fs = require('fs/promises');
+
+async function main() {
+    const Token =  hre.ethers.getContractFactory("Token");
+    token = await Token.deploy("100");
+    
+    const DEX = hre.ethers.getContractFactory("DEX");
+    dex = await DEX.deploy(token.target, 100);
+
+    await token.waitForDeployment();
+    await dex.waitForDeployment();
+
+    await writeDeploymentInfo(token,"TokenDeployment.json");
+    await writeDeploymentInfo(dex,"DexDeployment.json");
+}
+
+async function writeDeploymentInfo(contract,filename ="") {
+    const data = {
+        contract : {
+            address : contract.target,
+            signerAddress : await contract.runner.getAddress(),
+            abi : contract.interface.format()
+        }
+    }
+
+    const content = JSON.stringify(data,null,2);
+    await fs.writeFile(filename,content,{encoding: "utf-8"});
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+})
